35530169e57ea699902bfef8c63a23ec71b50b6d
Filippos Christianos <f.christianos@ed.ac.uk>
2022-09-21 16:58:34 +0100

    Add dot_config/nvim/lua/keymaps.lua Add dot_config/nvim/lua/options.lua Add dot_config/nvim/lua/plugins.lua

---
+++ dot_config/nvim/lua/plugins.lua
@@ -0,0 +1,56 @@
+
+local ensure_packer = function()
+  local fn = vim.fn
+  local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+  if fn.empty(fn.glob(install_path)) > 0 then
+    fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
+    vim.cmd [[packadd packer.nvim]]
+    return true
+  end
+  return false
+end
+
+local packer_bootstrap = ensure_packer()
+
+return require('packer').startup(function(use)
+  use 'wbthomason/packer.nvim'
+
+  -- colorscheme and statusline
+  use 'folke/tokyonight.nvim'
+  use {
+	'nvim-lualine/lualine.nvim',
+	requires = { 'kyazdani42/nvim-web-devicons', opt = true }
+  }
+  -- python stuff:
+  use {'psf/black', branch = 'stable' }
+  use 'brentyi/isort.vim'
+
+  -- general plugins:
+  use 'tpope/vim-commentary'
+  use 'tpope/vim-surround'
+  use 'tpope/vim-fugitive'
+  use 'tpope/vim-repeat'
+
+  -- lightspeed for fast moving in files
+  use 'ggandor/lightspeed.nvim'
+
+  -- telescope config:
+  use {
+	'nvim-telescope/telescope.nvim', tag = '0.1.0',
+	requires = { {'nvim-lua/plenary.nvim'} }
+  }
+  use {'nvim-telescope/telescope-fzf-native.nvim', run = 'make' }
+  
+  use {
+      'nvim-treesitter/nvim-treesitter',
+      run = function() require('nvim-treesitter.install').update({ with_sync = true }) end,
+  }
+
+  -- Automatically set up your configuration after cloning packer.nvim
+  -- Put this at the end after all plugins
+  if packer_bootstrap then
+    require('packer').sync()
+  end
+end)
+
+
