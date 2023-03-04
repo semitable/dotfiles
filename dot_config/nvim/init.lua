46b227fd232d63ec9326ab5cc33a088497b63a0f
Filippos Christianos <filippos.christianos@gmail.com>
2023-03-04 14:53:21 +0000

    Update .config/nvim/init.lua Add .config/nvim/lazy-lock.json Update .config/nvim/lua/plugins.lua

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -38,10 +38,10 @@ require("luasnip.loaders.from_lua").lazy_load({paths = "~/.config/nvim/luasnippe
 -- require("luasnip.loaders.from_lua").load({paths = "~/.config/nvim/LuaSnip/"})
 require("luasnip").config.set_config({ -- Setting LuaSnip config
 
-  -- Enable autotriggered snippets
+--   -- Enable autotriggered snippets
   enable_autosnippets = true,
 
-  -- Use Tab (or some other key if you prefer) to trigger visual selection
+--   -- Use Tab (or some other key if you prefer) to trigger visual selection
   store_selection_keys = "<Tab>",
 })
 
--- dot_config/nvim/lua/plugins.lua
+++ dot_config/nvim/lua/plugins.lua
@@ -1,68 +1,66 @@
-
-local ensure_packer = function()
-  local fn = vim.fn
-  local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
-  if fn.empty(fn.glob(install_path)) > 0 then
-    fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
-    vim.cmd [[packadd packer.nvim]]
-    return true
-  end
-  return false
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
 end
+vim.opt.rtp:prepend(lazypath)
 
-local packer_bootstrap = ensure_packer()
-
-return require('packer').startup(function(use)
-  use 'wbthomason/packer.nvim'
-
+local plugins = {
   -- colorscheme and statusline
-  use 'folke/tokyonight.nvim'
-  use {
-	'nvim-lualine/lualine.nvim',
-	requires = { 'kyazdani42/nvim-web-devicons', opt = true }
-  }
+  'folke/tokyonight.nvim',
+  {
+      'nvim-lualine/lualine.nvim',
+       dependencies = { 'kyazdani42/nvim-web-devicons' }
+  },
   -- stuff for languages I use:
   -- python:
-  use {'psf/black', branch = 'stable' }
-  use 'brentyi/isort.vim'
+  {
+      'psf/black',
+      branch = 'stable'
+  },
+  'brentyi/isort.vim',
   -- snipets:
-  use "L3MON4D3/LuaSnip"
+  "L3MON4D3/LuaSnip",
 
   -- LaTeX:
-  use 'lervag/vimtex'
+  'lervag/vimtex',
 
   -- VimWiki:
-  use 'vimwiki/vimwiki'
+  'vimwiki/vimwiki',
 
   -- beancount:
-  use 'nathangrigg/vim-beancount'
+  -- 'nathangrigg/vim-beancount',
 
   -- general plugins:
-  use 'tpope/vim-commentary'
-  use 'tpope/vim-surround'
-  use 'tpope/vim-fugitive'
-  use 'tpope/vim-repeat'
+  'tpope/vim-commentary',
+  'tpope/vim-surround',
+  'tpope/vim-fugitive',
+  'tpope/vim-repeat',
 
   -- lightspeed for fast moving in files
-  use 'ggandor/lightspeed.nvim'
+  'ggandor/lightspeed.nvim',
 
   -- telescope config:
-  use {
-	'nvim-telescope/telescope.nvim', tag = '0.1.0',
-	requires = { {'nvim-lua/plenary.nvim'} }
-  }
-  use {'nvim-telescope/telescope-fzf-native.nvim', run = 'make' }
-  
-  use {
+  {
+    'nvim-telescope/telescope.nvim', tag = '0.1.1',
+      dependencies = { 'nvim-lua/plenary.nvim' }
+  },
+  {
+      'nvim-telescope/telescope-fzf-native.nvim',
+      build = 'make' 
+  },
+  {
       'nvim-treesitter/nvim-treesitter',
-      run = function() require('nvim-treesitter.install').update({ with_sync = true }) end,
-  }
+      build = function() 
+          pcall(require('nvim-treesitter.install').update { with_sync = true }) 
+      end,
+  },
+}
 
-  -- Automatically set up your configuration after cloning packer.nvim
-  -- Put this at the end after all plugins
-  if packer_bootstrap then
-    require('packer').sync()
-  end
-end)
-
-
+require("lazy").setup(plugins)
