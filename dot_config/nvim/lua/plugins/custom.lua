ff2f1153d0173217ed88fb1d5dd0a257734979d0
Filippos Christianos <filippos.christianos@gmail.com>
2023-12-22 19:57:20 +0000

    Add .config/nvim/LICENSE Add .config/nvim/README.md Add .config/nvim/.gitignore Add .config/nvim/.neoconf.json Update .config/nvim/init.lua Update .config/nvim/lazy-lock.json Add .config/nvim/lazyvim.json Add .config/nvim/lua/config/autocmds.lua Add .config/nvim/lua/config/keymaps.lua Add .config/nvim/lua/config/lazy.lua Add .config/nvim/lua/config/options.lua Add .config/nvim/lua/plugins/custom.lua Add .config/nvim/lua/plugins/example.lua Add .config/nvim/stylua.toml

---
+++ dot_config/nvim/lua/plugins/custom.lua
@@ -0,0 +1,48 @@
+-- since this is just an example spec, don't actually load anything here and return an empty spec
+-- stylua: ignore
+
+-- every spec file under the "plugins" directory will be loaded automatically by lazy.nvim
+--
+-- In your plugin files, you can:
+-- * add extra plugins
+-- * disable/enabled LazyVim plugins
+-- * override the configuration of LazyVim plugins
+return {
+  -- add gruvbox
+  { "ellisonleao/gruvbox.nvim" },
+
+  -- Configure LazyVim to load gruvbox
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "gruvbox",
+    },
+  },
+
+  {
+    "telescope.nvim",
+    dependencies = {
+      "nvim-telescope/telescope-fzf-native.nvim",
+      build = "make",
+      config = function()
+        require("telescope").load_extension("fzf")
+      end,
+    },
+  },
+  -- add more treesitter parsers
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = {
+      ensure_installed = {
+        "bash",
+        "json",
+        "lua",
+        "markdown",
+        "markdown_inline",
+        "python",
+        "vim",
+        "yaml",
+      },
+    },
+  },
+}
