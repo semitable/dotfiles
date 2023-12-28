309401fb93188f522ed95132e810b8312592d2e1
Filippos Christianos <f.christianos@ed.ac.uk>
2023-12-28 13:37:46 +0000

    Update .config/nvim/lua/plugins/custom.lua

--- dot_config/nvim/lua/plugins/custom.lua
+++ dot_config/nvim/lua/plugins/custom.lua
@@ -9,7 +9,11 @@
 -- * override the configuration of LazyVim plugins
 return {
   -- add gruvbox
-  { "ellisonleao/gruvbox.nvim" },
+  {
+    "ellisonleao/gruvbox.nvim",
+    lazy = false,
+    priority = 1000,
+  },
 
   -- Configure LazyVim to load gruvbox
   {
